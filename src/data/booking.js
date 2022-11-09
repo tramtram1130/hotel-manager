6390e9a044f58e3d94b3c5a9127a5a08cb5db041
Dana Truong <tramtramphotos@gmail.com>
2022-11-09 12:43:34 -0600

    Add class test data for testing

---
+++ src/data/booking.js
@@ -0,0 +1,62 @@
+const bookingsData = [
+  {
+    id: "5fwrgu4i7k55hl6ta",
+    userID: 25,
+    date: "2022/01/11",
+    roomNumber: 9
+  },
+  {
+    id: "5fwrgu4i7k55hl7t8",
+    userID: 25,
+    date: "2022/01/25",
+    roomNumber: 10
+  },
+  {
+    id: "5fwrgu4i7k55hl790",
+    userID: 25,
+    date: "2023/11/16",
+    roomNumber: 22
+  },
+  {
+    id: "5fwrgu4i7k55hl7a2",
+    userID: 25,
+    date: "2023/12/11",
+    roomNumber: 11
+  },
+  {
+    id: "5fwrgu4i7k55hl87m",
+    userID: 7,
+    date: "2022/02/08",
+    roomNumber: 8
+  },
+  {
+    id: "5fwrgu4i7k55hl79r",
+    userID: 7,
+    date: "2022/02/19",
+    roomNumber: 2
+  },
+  {
+    id: "5fwrgu4i7k55hl6tk",
+    userID: 7,
+    date: "2023/11/27",
+    roomNumber: 20
+  },
+  {
+    id: "5fwrgu4i7k55hl7xe",
+    userID: 7,
+    date: "2023/12/22",
+    roomNumber: 5
+  },
+  {
+    id: "5fwrgu4i7k55hl7f7",
+    userID: 42,
+    date: "2022/02/11",
+    roomNumber: 2
+  },
+  {
+    id: "5fwrgu4i7k55hl6xq",
+    userID: 42,
+    date: "2022/02/14",
+    roomNumber: 6
+  }
+]
